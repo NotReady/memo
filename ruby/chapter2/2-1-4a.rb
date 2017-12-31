6dbc84eefa59338c599fe20294b3b40ee133597a
NotReady <loreley.lala@gmail.com>
2017-12-31 16:22:48 +0900

    配列の基礎

---
+++ ruby/chapter2/2-1-4a.rb
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'elephant']
+puts animals[0]
+animals[1] = 'bat'
+puts animals[1]
+
+puts animals
---
+++ ruby/chapter2/2-1-4b.rb
@@ -0,0 +1,8 @@
+animals = ['dog', 'cat', 'elephant']
+puts animals[5]	#=> nil
+animals[5] = 'whale'	#=> 6番目を作成
+puts animals
+
+cabinet = [123, 'hello', Time.now, ['apple', 'orange']]
+puts cabinet
+
